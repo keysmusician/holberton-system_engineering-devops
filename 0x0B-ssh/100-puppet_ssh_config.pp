2f83619682829b3d7812de68257afc397e998435
keysmusician <justin.masayda@gmail.com>
2021-08-15 04:57:45 +0000

    Add comment

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,4 @@
+# Create an SSH configuration file
 file { '/home/vagrant/.ssh/config':
   ensure  => file,
   content => "# SSH configuration file\nIdentityFile ~/.ssh/holberton\nPasswordAuthentication no",
