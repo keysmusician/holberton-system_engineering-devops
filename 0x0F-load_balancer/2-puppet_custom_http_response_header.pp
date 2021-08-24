dee81fb611c7c9667c791a2243f270d0709d5ac2
keysmusician <justin.masayda@gmail.com>
2021-08-24 08:45:51 +0000

    Require html directory and Nginx service running

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
-#!/usr/bin/puppet
+# Puppet manifest
 # Configure a Nginx server with a custom HTTP header and two web pages.
 
-# Install HAProxy
-package { 'haproxy':
+# Install Nginx
+package { 'nginx':
   ensure => installed,
 }
 
@@ -13,31 +13,41 @@ exec { 'hostname -b $(hostnamectl --static)':
 
 # Configure Nginx server
 file { '/etc/nginx/sites-available/default':
+	ensure  => file,
 	content => '
-	server {
-		listen 80;
-		listen [::]:80 default_server;
-		root   /var/www/html;
-		index  index.html;
-		location /redirect_me {
-			return 301 https://www.youtube.com/c/JustinMasayda;
+		server {
+			listen 80;
+			listen [::]:80 default_server;
+			root   /var/www/html;
+			index  index.html;
+			location /redirect_me {
+				return 301 https://www.youtube.com/c/JustinMasayda;
+			}
+			error_page 404 /404.html;
+			add_header X-Served-By $HOSTNAME;
 		}
-		error_page 404 /404.html;
-		add_header X-Served-By $HOSTNAME;
-	}
-	',
+		',
 }
 
 # Create webpages
+# website directory
+file { ['/var/www', '/var/www/html']:
+	ensure => directory,
+}
+
 # Index
 file { '/var/www/html/index.html':
+	ensure  => file,
 	content => "Holberton School for the win!",
 }
 
 # 404
 file { '/var/www/html/404.html':
- content => "Ceci n'est pas une page",
+	ensure  => file,
+	content => "Ceci n'est pas une page",
 }
 
 # Launch Nginx server
-service { 'nginx': }
+service { 'nginx':
+	ensure => running,
+}
