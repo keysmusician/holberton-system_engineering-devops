d52b802ca4767fc19a1b2c7aa7c15ddfb903fe4c
keysmusician <justin.masayda@gmail.com>
2021-09-13 19:28:42 +0000

    Fix output 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,21 +7,21 @@ from sys import argv
 if __name__ == "__main__":
     employee_id = argv[1]
     api = 'https://jsonplaceholder.typicode.com/'
+
     endpoint = 'users/{}'.format(employee_id)
     employee = requests.get(api + endpoint).json()
 
     endpoint = 'todos?userId={}'.format(employee_id)
-    employee_tasks = requests.get(api + endpoint).json()
+    tasks = requests.get(api + endpoint).json()
 
     employee_name = employee.get("name")
-    employee_tasks_count = len(employee_tasks)
+    task_count = len(tasks)
 
-    endpoint = 'todos'
-    all_tasks_count = len(requests.get(api + endpoint).json())
+    completed_tasks = [task for task in tasks if task.get("completed")]
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, employee_tasks_count, all_tasks_count
+        employee_name, len(completed_tasks), task_count
         )
     )
-    for task in employee_tasks:
+    for task in completed_tasks:
         print("\t " + task.get("title"))
