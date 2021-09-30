aa5bcf97c9d0697cbcdd1be32266f2e55b81392a
Justin Masayda <justin.masayda@gmail.com>
2021-09-29 20:04:30 -0400

    add /

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fixes a filename extention typo
+# Fixes a filename extension typo
 exec { 'rename file':
-  command => 'sudo sed -i "s/.phpp/.php" var/www/html/wp-settings.php',
+  command => 'sudo sed -i "s/.phpp/.php" /var/www/html/wp-settings.php',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
 }
