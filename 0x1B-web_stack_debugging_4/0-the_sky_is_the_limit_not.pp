e8fd9bd1d0d41ced1c03a428da8f2c7cadf047eb
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:12:10 -0400

    Update comments

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-#increase allowed traffic
+# Increase open file limit
 exec {'increase traffic':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
