9f15c247e5bde79f97ea19595f7d0e419a14922f
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:02:38 -0400

    Test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
-exec {'increase open file limit':
+#increase allowed traffic
+exec {'increase traffic':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  notify  => Service['nginx'],
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
-exec { 'restart nginx':
+#restart nginx
+exec { 'nginx restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
