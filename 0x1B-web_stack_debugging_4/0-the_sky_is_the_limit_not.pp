ee2e8c44e2d6f5fbc0ad05d9c4708a7a590c8fa5
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:13:36 -0400

    Fully qualified path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,9 @@
 # Increase open file limit
 exec {'increase traffic':
   onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  command => '/bin/sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
 }
-#restart nginx
+# Restart Nginx
 exec { 'nginx restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
