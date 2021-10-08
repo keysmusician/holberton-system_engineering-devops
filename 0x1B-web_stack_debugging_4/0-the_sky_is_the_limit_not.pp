541690a67a80b91caf6a748632b11f0aaeee76d3
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:09:21 -0400

    Revert "Update exec"
    This reverts commit 1b8699f1ee5f90adf339a0a3a2f565f9e0580ed2.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Increase open file limit
-exec {'increase open file limit':
+exec {'increase limit':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
 # Restart Nginx
-exec { 'nginx restart':
+exec { 'restart nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
