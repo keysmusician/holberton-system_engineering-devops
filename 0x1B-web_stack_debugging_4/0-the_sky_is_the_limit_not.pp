2591e4ff67538a7744df185e6afa66bd3528c110
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:14:39 -0400

    Revert

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # Increase open file limit
 exec {'increase traffic':
   onlyif  => 'test -e /etc/default/nginx',
-  command => '/bin/sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 # Restart Nginx
 exec { 'nginx restart':
