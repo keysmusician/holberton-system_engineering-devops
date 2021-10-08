a9cd93d6c143ad7fe7d50ea58babaf11bc2decea
Justin Masayda <justin.masayda@gmail.com>
2021-10-07 23:17:52 -0400

    Revert exec title change

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Increase open file limit
-exec {'increase limit':
+exec {'increase traffic':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
